7f3afeb4e4028f42dd9ca2a6e2baf3356d8ad22c
hars123 <harshitshukla4513@gmail.com>
2025-05-27 11:18:31 +0530

    add

---
+++ assignment27may/Array.js
@@ -0,0 +1,7 @@
+const nums = [1, 2, 3];
+const doubled = nums.map(n => n * 2);
+console.log(doubled); // [2, 4, 6]
+
+const words = ['hello', 'world'];
+const caps = words.map(w => w.toUpperCase());
+console.log(caps); // ['HELLO', 'WORLD']
